903c61c9a89e97cfe6aa602624f974ec71eef19a
SLevente001 <221684507+slevente713@users.noreply.github.com>
2026-07-27 10:09:19 +0200

    2.2

---
+++ DeleteTemporaryFiles2_2.ps1
@@ -0,0 +1,88 @@
+Write-Host "Appdata\Local\Temp tartalmának törlése... Removing files in Appdata\Local\Temp..." -ForegroundColor Cyan
+rm "C:\Users\Default\AppData\Local\Temp\*" -Recurse -Force -ErrorAction SilentlyContinue
+rm "$env:USERPROFILE\AppData\Local\Temp\*" -Recurse -Force -ErrorAction SilentlyContinue
+Write-Host "Appdata\Local\Temp tartalma sikeresen törölve. Files in Appdata\Local\Temp were successfully removed." -ForegroundColor Green
+Write-Host "Windows\Temp tartalmának törlése... Removing files in Windows\Temp..." -ForegroundColor Cyan
+rm "C:\Windows\Temp\*" -Recurse -Force -ErrorAction SilentlyContinue
+Write-Host "Windows\Temp tartalma sikeresen törölve. Files in Windows\Temp were successfully removed." -ForegroundColor Green
+Write-Host "Prefetch fájlok törlése... Removing Prefetch files..." -ForegroundColor Cyan
+rm "$env:SystemRoot\Prefetch\*" -Recurse -Force -ErrorAction SilentlyContinue
+Write-Host "Prefetch fájlok sikeresen törölve. Prefetch files were successfully removed." -ForegroundColor Green
+Write-Host "Windows Update maradványok törlése... Removing Windows Update junk files..." -ForegroundColor Cyan
+rm "C:\Windows\SoftwareDistribution\Download\*" -Recurse -Force -ErrorAction SilentlyContinue
+Write-Host "Windows Update maradványok sikeresen törölve. Windows Update junk were successfully removed." -ForegroundColor Green
+Write-Host "Delivery optimization gyorsítótár törlése... Removing Delivery optimization cache..." -ForegroundColor Cyan
+rm "C:\Windows\DeliveryOptimization\Cache\*" -Recurse -Force -ErrorAction SilentlyContinue
+Write-Host "A Delivery optimization gyorsítótár sikeresen törlésre került. Delivery optimization cache were successfully removed." -ForegroundColor Green
+Write-Host "Windows Error Reporting gyorsítotár törlése... Removing Windows Error Reporting cache..." -ForegroundColor Cyan
+rm "C:\Windows\Minidump\*" -Recurse -Force -ErrorAction SilentlyContinue
+rm "$env:LOCALAPPDATA\CrashDumps\*" -Recurse -Force -ErrorAction SilentlyContinue
+Write-Host "Windows Error Reporting gyorsítotár sikeresen törlésre került. Windows Error Reporting cache were successfully removed." -ForegroundColor Green
+$question1 = Read-Host "Szeretnéd törölni az Offline Web Pages mappa tartalmát? [Y/N] Would you like to delete the files in Offline Web Pages Folder? [Y/N]"
+if ($question1 -match "y") {
+    $path1 = "C:\Windows\Offline Web Pages"
+    if (Test-Path $path1) {
+        rm "$path1\*" -Recurse -Force -ErrorAction SilentlyContinue
+        Write-Host "Az Offline Web Pages mappa tartalma sikeresen törlődött. The files in the Offline Web Pages folder were successfully removed." -ForegroundColor Green
+    }
+} else {
+    Write-Host "Megszakítottad az Offline Web Pages mappa tartalmának törlését. You cancelled removing the files in the Offline Web Pages folder." -ForegroundColor Yellow
+}
+$question2 = Read-Host "Szeretnéd törölni a Windows LOG fájlokat? [Y/N] Would you like to delete the Windows LOG files? [Y/N]"
+if ($question2 -match "y") {
+    $path2 = "C:\Windows\LogFiles"
+    if (Test-Path $path2) {
+        rm "$path2\*" -Recurse -Force -ErrorAction SilentlyContinue
+        Write-Host "A Windows LOG fájlok sikeresen törlésre kerültek. Windows LOG files were successfully removed." -ForegroundColor Green
+    }
+} else {
+    Write-Host "Megszakítottad a Windows LOG fájlok törlését. You cancelled removing Windows LOG files." -ForegroundColor Yellow
+}
+
+$question3 = Read-Host "Szeretnéd kikapcsolni a SysMain (vagy SuperFetch) szolgáltatást hogy a Prefetch mappába ne generálódjon több fájl? (HDD-n nem javasolt) [Y/N] Would you like to turn off SysMain (or SuperFetch) to don't generate more files to the Prefetch folder? (Not recomended with HDDs.) [Y/N]"
+if ($question3 -match "y") {
+    $services = @(
+    "SysMain"           # SysMain (Superfetch)
+)
+
+foreach ($service in $services) {
+    $serviceObj = Get-Service -Name $service -ErrorAction SilentlyContinue
+    
+    if ($serviceObj) {
+        try {
+            if ($serviceObj.Status -ne 'Stopped') {
+                Stop-Service -Name $service -Force -Confirm:$false -ErrorAction SilentlyContinue
+            }
+            Set-Service -Name $service -StartupType Disabled
+        }
+        catch {
+
+        }
+    }
+}
+        Write-Host "A SysMain (SuperFetch) szolgáltatás sikeresen letiltva, és leállítva. The service SysMain (SuperFetch) is successfully disabled and stopped" -ForegroundColor Green
+    }
+else {
+    $services = @(
+    "SysMain"           # SysMain (Superfetch)
+)
+
+foreach ($service in $services) {
+    $serviceObj = Get-Service -Name $service -ErrorAction SilentlyContinue
+    
+    if ($serviceObj) {
+        try {
+            if ($serviceObj.Status -ne 'Running') {
+                Start-Service -Name $service -Force -Confirm:$false -ErrorAction SilentlyContinue
+            }
+            Set-Service -Name $service -StartupType Automatic
+        }
+        catch {
+            
+        }
+    }
+}
+    Write-Host "Megszakítottad a Sysmain (SuperFetch) kikapcsolását. You cancelled turning off SysMain (SuperFetch)." -ForegroundColor Yellow
+}
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+pause
